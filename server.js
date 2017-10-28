72e5d880d43fd205cd3eb6c476fd945928a9a040
David Linley <dtlinley@gmail.com>
2017-10-27 22:12:14 -0400

    Add template and CSS
    Basic template and very basic CSS to get started.

--- server.js
+++ server.js
@@ -1,33 +1,42 @@
 const Hapi = require('hapi');
-const handlebars = require('handlebars');
+const Handlebars = require('handlebars');
+const Vision = require('vision');
+const Inert = require('inert');
 
-// Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({
   host: 'localhost',
   port: 8000,
 });
 
-server.register(require('vision'), () => {
+server.register([Vision, Inert], () => {
   server.views({
     engines: {
-      html: handlebars,
+      html: Handlebars,
     },
     relativeTo: __dirname,
     path: 'templates',
   });
 
-  // Add the route
   server.route({
     method: 'GET',
-    path: '/hello',
-    handler: (request, reply) => reply('hello world'),
+    path: '/',
+    handler: (request, reply) => reply.view('index'),
   });
 
-  // Start the server
-  server.start((ServerErr) => {
-    if (ServerErr) {
-      throw ServerErr;
+  server.route({
+    method: 'GET',
+    path: '/styles/{filename*}',
+    handler: {
+      directory: {
+        path: 'styles',
+      },
+    },
+  });
+
+  server.start((serverErr) => {
+    if (serverErr) {
+      throw serverErr;
     }
     console.log('Server running at:', server.info.uri); // eslint-disable-line no-console
   });
