1f8c18946433a60289117f7ec29fa1025908ec58
meeschka <michpitts@hotmail.com>
2017-11-30 19:09:28 -0500

    Persist rsvp list

--- test/server.js
+++ test/server.js
@@ -1,9 +1,39 @@
 const { assert } = require('chai');
+const server = require('../server');
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal(-1, [1, 2, 3].indexOf(4));
+describe('Server', () => {
+  describe('#POST /api/rsvp', () => {
+    it('should persist rsvp details', (done) => {
+      const promise = server.inject({
+        method: 'POST',
+        url: '/api/rsvp',
+        payload: {
+          attending: true,
+          guestName: 'Luke Skywalker',
+          allergies: 'Many',
+        },
+      });
+      promise.then(() => {
+        assert(server.rsvpList.length, 1);
+        done();
+      });
+    });
+
+    it('should reply with success message', (done) => {
+      const promise = server.inject({
+        method: 'Post',
+        url: '/api/rsvp',
+        payload: {
+          attending: true,
+          guestName: 'Luke Skywalker',
+          allergies: 'Many',
+        },
+      });
+      promise.then((response) => {
+        assert.isOk(response);
+        assert.isOk(response.payload);
+        done();
+      });
     });
   });
 });
